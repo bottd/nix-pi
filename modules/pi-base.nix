a4dacbab323d3ce7e7bc7f4116baa948225aa54f
Drake Bott <bott.da@gmail.com>
2025-09-15 09:59:20 -0500

    Update

--- modules/pi-base.nix
+++ modules/pi-base.nix
@@ -41,7 +41,6 @@ in
 
     hardware = {
       firmware = with pkgs; [ linux-firmware wireless-regdb ];
-      raspberry-pi."4".apply-overlays-dtmerge.enable = true;
       deviceTree = { enable = true; filter = "*rpi-4-*.dtb"; };
       bluetooth.enable = true;
     };
